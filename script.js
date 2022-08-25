b92a643621a93f19f10df8f617fe6e7e33c5763a
Anzelo7 <andrea.anzellotti7@gmail.com>
2022-08-25 19:02:11 +0200

    push

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log('JS GOD')
+const grid = document.getElementById('grid');
+
+for (let squareIndex = 1; squareIndex <= 100; squareIndex++) {
+    const element = document.createElement('div');
+
+    element.append(squareIndex.toFixed());
+    element.className = 'bg-green big';
+    //   element.classList.add('bg-green');
+
+    element.style.textAlign = 'center';
+    element.style.display = 'flex';
+    element.style.flexDirection = 'column';
+    element.style.justifyContent = 'center';
+
+
+
+    element.addEventListener('mouseleave', function () {
+        console.log('esco da ' + squareIndex);
+        element.style.backgroundColor = 'greenyellow';
+    });
+
+
+    grid.append(element);
+}
+
+let squareIndextre = squareIndex * 3
+let squareIndexcinque = squareIndex * 5
+
+switch (multiplisquareIndex) {
+    case squareIndextre:
+        element.src.div = 'Fizz'
+        break;
+    case squareIndexcinque:
+        element.src.div = 'Buzz'
+        break;
+}
